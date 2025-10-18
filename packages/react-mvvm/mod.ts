d36bf32db4f5c16415e5a81110c8b80b292f336c
Jonathan Kilzi <jkilzi@redhat.com>
2025-10-18 21:24:39 +0300

    feat: export additional types for improved core functionality
    - Added export for IDisposable and IListenable types in mod.ts to enhance type management and usability across the package.

--- packages/react-mvvm/mod.ts
+++ packages/react-mvvm/mod.ts
@@ -2,3 +2,5 @@ export * from "./internal/react/use_view_model.ts";
 export * from "./internal/core/change_notifier.ts";
 export * from "./internal/core/observable.ts";
 export * from "./internal/core/result.ts";
+export type * from "./internal/core/disposable.ts";
+export type * from "./internal/core/listenable.ts";
