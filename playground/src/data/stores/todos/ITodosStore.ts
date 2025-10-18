8181db4eda21f00a8aa85d0d7d94a49b2c67f6aa
Jonathan Kilzi <jkilzi@redhat.com>
2025-10-18 14:46:49 +0300

    refactor: update ITodosStore method signatures for consistency
    - Changed method signatures in the ITodosStore interface to use concise syntax for better readability and consistency across the codebase.

--- playground/src/data/stores/todos/ITodosStore.ts
+++ playground/src/data/stores/todos/ITodosStore.ts
@@ -2,7 +2,7 @@ import type { Todo } from "../../../domain/models/Todo.ts";
 
 export interface ITodosStore {
   get todos(): Todo[];
-  addTodo: (todo: Todo) => void;
-  removeTodo: (id: Todo['id']) => void;
-  toggleTodo: (id: Todo['id']) => void;
+  addTodo(todo: Todo): void;
+  removeTodo(id: Todo['id']): void;
+  toggleTodo(id: Todo['id']): void;
 }
