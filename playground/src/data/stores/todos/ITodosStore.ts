6fb6a55fd9f93f0a940e42d751975d429e1334a3
Jonathan Kilzi <jkilzi@redhat.com>
2025-10-18 14:30:39 +0300

    refactor: update ITodosStore interface to use getter for todos property
    - Changed the todos property in ITodosStore from a direct array to a getter method for improved encapsulation and data access.
    - Maintained the existing addTodo and removeTodo methods, and restored the toggleTodo method for consistency.

--- playground/src/data/stores/todos/ITodosStore.ts
+++ playground/src/data/stores/todos/ITodosStore.ts
@@ -1,8 +1,8 @@
 import type { Todo } from "../../../domain/models/Todo.ts";
 
 export interface ITodosStore {
-  todos: Todo[];
+  get todos(): Todo[];
   addTodo: (todo: Todo) => void;
-  toggleTodo: (id: number) => void;
   removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
 }
