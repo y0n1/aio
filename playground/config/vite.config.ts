1a4878f5b854b17da62b3ecc935480bdf159aad0
y0n1 <y0n1@outlook.com>
2025-10-19 12:45:48 +0300

    chore: remove denoWorkspace plugin from vite.config.ts and update deno.jsonc dependencies
    - Removed the @y0n1/vite-plugin-deno-workspace import and its usage in vite.config.ts to streamline the configuration.
    - Updated deno.jsonc to exclude the denoWorkspace dependency, simplifying the project setup.

--- playground/config/vite.config.ts
+++ playground/config/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import denoWorkspace from "@y0n1/vite-plugin-deno-workspace";
 import path from "node:path";
 
 // https://vite.dev/config/
@@ -16,9 +15,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    denoWorkspace({
-      workspacePath: path.resolve("..", "deno.jsonc"),
-    }),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
